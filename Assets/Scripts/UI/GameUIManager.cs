b0c744d18401fd3562fe7393b2837a985d2ace72
Likeaalien <necatasic@gmail.com>
2026-02-27 19:53:17 +0100

    ValidateNick

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameUIManager : MonoBehaviour
 {
@@ -7,18 +8,27 @@ public class GameUIManager : MonoBehaviour
     public GameObject panel_hud;
     public TMP_InputField player_name_field;
     public Player player;
+    public Button play_button;
 
     void Start()
     {
         panel_start_menu.SetActive(true);
         panel_hud.SetActive(false);
+
+        player_name_field.onValueChanged.AddListener(ValidateNick);
+
+        ValidateNick(player_name_field.text);
     }
 
     public void StartGame()
     {
         player.player_nickname = player_name_field.text;
-        Debug.Log(player.player_nickname);
+        
         panel_start_menu.SetActive(false);
         panel_hud.SetActive(true);
+    }
+    private void ValidateNick(string text)
+    {
+        play_button.interactable = text.Length >= 3 && text.Length <= 12;
     }
 }
